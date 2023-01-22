968716e8ff71a0537a3887f5a06fce8fb3532cb4
nksudarshannk <109651976+nksudarshannk@users.noreply.github.com>
2023-01-22 16:23:52 +0530

    handlename

--- s11.py
+++ s11.py
@@ -1,3 +1,55 @@
-a=10
-b=20
-print(a+b)
+import{ useState }from "react";
+import "./app.css"
+export default function Form(){
+  
+    const[name,setName]=useState("")
+    const[email,setEmail]=useState("")
+    const[password,setPassword]=useState("")
+    const[submitted,setSubmitted]=useState(false)
+  
+    const handleName =(e)=>{
+      setName(e.target.value);
+    };
+    const handleEmail=(e)=>{
+      setEmail(e.target.value);
+    };
+    const handlePassword=(e)=>{
+      setPassword(e.target.value);
+    };
+    const handleSubmitted=(e)=>{
+      e.preventDefault();
+      if(name===""||email===""||password===""){
+        alert("enter the all the fields");
+      }
+      else{    
+        setSubmitted(true);
+      }
+    };
+    const successMeassage=()=>{
+        if(submitted)
+      return(
+        <div className="sucess">
+          <h1>user {name} sucessfully registered</h1>
+        </div>
+      );
+    }
+    return(
+      <div className="form">
+        <div>
+          <h1>user registration</h1>
+        </div>
+        <div className="messages">
+        {successMeassage()}
+        </div>
+        <form>
+          <label>name</label>
+          <input type="text" onChange={handleName}/>
+          <label>Email</label>
+          <input type="email" onChange={handleEmail}/>
+          <label>password</label>
+          <input type="password" onChange={handlePassword}/>
+          <input type="submit" value="submit" onClick={handleSubmitted}/>
+        </form>
+      </div>
+    );
+  }
